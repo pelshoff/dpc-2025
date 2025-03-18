fd67895d6a397c8b315465b722aaefc0f701d3f8
Pim Elshoff <pim.elshoff@gosuperscript.com>
2025-03-18 13:53:14 +0100

    Create manual review test case

--- modules/Company/tests/Unit/CompanyValidatorTest.php
+++ modules/Company/tests/Unit/CompanyValidatorTest.php
@@ -24,4 +24,21 @@ final class CompanyValidatorTest extends TestCase
         $officers = [new Officer('William of Orange'), new Officer('Not William of Orange')];
         $this->assertFalse(new CompanyValidator($client)->verify('abc123', $officers));
     }
+
+    #[Test]
+    public function manual_review_if_an_officer_almost_matches(): void
+    {
+        $client = $this->getMockBuilder(CompaniesHouseClient::class)->getMock();
+        $client->expects($this->any())->method('getCompanyProfile')->willReturn('Profile');
+        $client->expects($this->any())->method('listOfficers')->willReturn([
+            [
+                'name' => 'William of Orange',
+            ],
+            [
+                'name' => 'William of Green',
+            ]
+        ]);
+        $officers = [new Officer('William of Orange'), new Officer('William of Gren')];
+        $this->assertEquals("maybe", new CompanyValidator($client)->verify('abc123', $officers));
+    }
 }
